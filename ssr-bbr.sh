a6229c27c841e937a82240aea18a8911b10be084
HenryHandsome <32134376+HenryHandsome@users.noreply.github.com>
2020-02-28 02:05:08 +0800

    Update ssr-bbr.sh

--- ssr-bbr.sh
+++ ssr-bbr.sh
@@ -2,7 +2,7 @@
 wget https://raw.githubusercontent.com/HenryHandsome/vvvpppsss/master/sssrrr.sh
 chmod +x sssrrr.sh
 ./sssrrr.sh
-
 wget https://raw.githubusercontent.com/HenryHandsome/vvvpppsss/master/bbbrrr.sh
 chmod +x bbbrrr.sh
 ./bbbrrr.sh
+reboot
