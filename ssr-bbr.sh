d4ee51fa43dd541cebff32ae21396bf48aa4a036
HenryHandsome <32134376+HenryHandsome@users.noreply.github.com>
2020-02-28 13:52:22 +0800

    Update ssr-bbr.sh

--- ssr-bbr.sh
+++ ssr-bbr.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
+yum -y install expect
 wget https://raw.githubusercontent.com/HenryHandsome/vvvpppsss/master/sssrrr.sh
 chmod +x sssrrr.sh
 ./sssrrr.sh
