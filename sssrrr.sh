0c1e1266805b837423b665ea2942aa71e8acc18c
HenryHandsome <32134376+HenryHandsome@users.noreply.github.com>
2020-02-28 17:19:17 +0800

    Update sssrrr.sh

--- sssrrr.sh
+++ sssrrr.sh
@@ -2,9 +2,9 @@
 
 spawn ./shadowsocks-all.sh
 set timeout 300
-expect "*Default Shadowsocks-Python):" {send "2"}
-expect "*teddysun.com):" {send "zxh"}
-expect "*Default port:*" {send "12345"}
+expect "*Default Shadowsocks-Python):" {send "2\n"}
+expect "*teddysun.com):" {send "zxh\n"}
+expect "*Default port:*" {send "12345\n"}
 expect "*aes-256-cfb):" {send "\n"}
 expect "*(Default: origin):" {send "\n"}
 expect "*Default: plain):" {send "\n"}
