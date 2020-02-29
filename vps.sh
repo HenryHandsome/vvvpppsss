a806a767168e3e44ce578026db3659b98cd7dbae
HenryHandsome <32134376+HenryHandsome@users.noreply.github.com>
2020-02-29 14:26:17 +0800

    Create vps.sh

---
+++ vps.sh
@@ -0,0 +1,6 @@
+wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+chmod +x shadowsocks-all.sh
+wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh
+chmod +x bbr.sh
+./shadowsocks-all.sh
+./bbr.sh
